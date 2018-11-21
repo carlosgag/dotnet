11956138cad2fd5893dffa05c366e279fb58d1db
Carlos Gagliardi <carlos.gagliardi@dynamizeit.com>
2018-11-21 16:15:24 -0300

    ultimos cambios

---
+++ DotNet/Main/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //p.threads();
+            SharingVars s = new SharingVars();
+            s.doWork();
+            Console.ReadLine();
+        }
+
+        public Task<string> GetHtmlAsync()
+        {
+            HttpClient client = new HttpClient();
+            return client.GetStringAsync("http://www.dotnetfoundation.org");
+        }
+
+        public async Task<string> GetFirstCharactersCountAsync(int count)
+        {
+            string result = "";
+            HttpClient client = new HttpClient();
+            string page = await client.GetStringAsync("http://www.dotnetfoundation.org");
+            if (count > page.Length)
+            {
+                result = page;
+            }
+            else
+            {
+                result = page.Substring(0, count);
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+            return result;
+        }
+
+        public void threads()
+        {
+            ThreadStart job = new ThreadStart(ThreadJob);
+            Thread thread = new Thread(job);
+            thread.Start();
+            for (int i = 0; i<5;i++)
+            {
+                Console.WriteLine("Main Thread: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void ThreadJob()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Other Thread: {0}", i);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ DotNet/Main/SharingVars.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class SharingVars
+    {
+        private string _threadOutput = "";
+        private bool _stopThreads = false;
+
+        private void DisplayThread1()
+        {
+            while (_stopThreads == false)
+            {
+                Console.WriteLine("Display Thread 1");
+                _threadOutput = "Hello Thread 1";
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1 output --> {0}", _threadOutput);
+            }
+        }
+
+        private void DisplayThread2()
+        {
+            while (_stopThreads == false)
+            {
+                Console.WriteLine("Display Thread 2");
+                _threadOutput = "Hello Thread 2";
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2 output --> {0}", _threadOutput);
+            }
+        }
+
+        public void doWork()
+        {
+            Thread thread1 = new Thread(new ThreadStart(DisplayThread1));
+            Thread thread2 = new Thread(new ThreadStart(DisplayThread2));
+
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+}
